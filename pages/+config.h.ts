65cd59f1a21e7df4a65c82ea6af7a18efefb1de1
Dayana Ilieva <dayana.ilieva@1forfit.com>
2024-05-27 15:47:52 +0300

    Test official boilerplate from vite

--- pages/+config.h.ts
+++
@@ -1,8 +0,0 @@
-import type { Config } from 'vike/types'
-import vikeReact from 'vike-react'
-import { PageShell } from '#/layouts/PageShell'
-
-export default {
-  extends: vikeReact,
-  Layout: PageShell,
-} satisfies Config
